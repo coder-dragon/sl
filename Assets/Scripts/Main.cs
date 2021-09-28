7640859c23a29a49f23caee4ea4d06231a739012
李文龙 <783929897@qq.com>
2021-09-28 18:57:15 +0800

    新增图表插件，新增textmeshpro

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -16,7 +16,16 @@ namespace SL
             var assistant = new Assistant();
             assistant.LoadConfig(Application.streamingAssetsPath + "/Config/配置.txt");
             assistant.Start();
-            StartCoroutine(Refresh(assistant));
+            //StartCoroutine(Refresh1(assistant));
+        }
+
+        private void InitTabHeaders(Assistant assistant)
+        {
+            for (int i = 0; i < assistant.Config.gift.groups.Length; i++)
+            {
+                var group = assistant.Config.gift.groups[i];
+                //TODO:
+            }
         }
 
         private IEnumerator Refresh(Assistant assistant)
@@ -51,6 +60,56 @@ namespace SL
                 foreach (var pair in assistant.Groups.Records)
                 {
                     buffer.Append($"{pair.Key}={pair.Value}\r\n");
+                }
+                addText(buffer.ToString());
+
+                var begin = DateTime.Now;
+                while (DateTime.Now - begin < TimeSpan.FromSeconds(1))
+                {
+                    if (Input.GetKey(KeyCode.R))
+                    {
+                        assistant.Statistic.Reset();
+                        assistant.Groups.Reset();
+                    }
+
+                    yield return null;
+                }
+            }
+        }
+        
+        private IEnumerator Refresh1(string groupName, Assistant assistant)
+        {
+            while (true)
+            {
+                clearText();
+                
+                var buffer = new StringBuilder();
+                
+                buffer.Clear();
+                foreach (var pair in assistant.Groups.Records)
+                {
+                    buffer.Append($"{pair.Key}={pair.Value}\r\n");
+                }
+                addText(buffer.ToString());
+                
+                
+                buffer.AppendLine("最新播报：");
+                buffer.AppendLine();
+                if (!string.IsNullOrEmpty(assistant.LastError))
+                    buffer.AppendLine(assistant.LastError);
+                var messages = assistant.Statistic.GetTop();
+                foreach (var message in messages)
+                {
+                    buffer.AppendLine(message.ToString());
+                }
+                addText(buffer.ToString());
+
+                buffer.Clear();
+                buffer.AppendLine("统计：" + assistant.Statistic.Total);
+                buffer.AppendLine();
+                foreach (var pair in assistant.Statistic.Items)
+                {
+                    buffer.AppendFormat("{0}={1}\r\n", pair.Key, pair.Value);
                 }
                 addText(buffer.ToString());
 
